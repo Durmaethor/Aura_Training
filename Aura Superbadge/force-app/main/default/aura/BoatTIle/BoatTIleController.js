c845a016f396021fe068ae4d1cc9237a60916abd
Ryan Kramer <rkramer613@gmail.com>
2021-09-02 11:24:11 -0600

    Added multiple components

--- Aura Superbadge/force-app/main/default/aura/BoatTIle/BoatTIleController.js
+++ Aura Superbadge/force-app/main/default/aura/BoatTIle/BoatTIleController.js
@@ -9,6 +9,21 @@
         boatselect.fire();
 
         var boatselected = $A.get("e.c:BoatSelected");
+        boatselected.setParams({
+            "boat" : selectedBoat
+        });
+        boatselected.fire();
+        debugger;
+
+        var plotMapMarkerEvent = $A.get("e.c:PlotMapMarker");
+        plotMapMarkerEvent.setParams({
+            "sObjectId" : selectedBoat.Id,
+            "lat" : selectedBoat.Geolocation__Latitude__s,
+            "long" : selectedBoat.Geolocation__Longitude__s,
+            "label" : selectedBoat.Name
+        });
+        plotMapMarkerEvent.fire();
 
     }
+    
 })
