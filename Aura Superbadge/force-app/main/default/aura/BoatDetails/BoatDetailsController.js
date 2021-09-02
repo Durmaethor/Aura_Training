3c79e73e04f54101af66b15cba7c2750038b004a
Ryan Kramer <rkramer613@gmail.com>
2021-09-02 12:49:18 -0600

    added boat details

--- Aura Superbadge/force-app/main/default/aura/BoatDetails/BoatDetailsController.js
+++ Aura Superbadge/force-app/main/default/aura/BoatDetails/BoatDetailsController.js
@@ -1,5 +1,30 @@
 ({
-    myAction : function(component, event, helper) {
+    onBoatSelected : function(component, event, helper) {
+
+        var boat = event.getParam('boat');
+        component.set("v.id", boat.Id);
+        component.find("service").reloadRecord();
+
+    },
+
+    onRecordUpdated : function(component, event, helper) {
+
+        var boatReviewsCmp = component.find("boatReviewsCmp");
+        if(boatReviewsCmp) {
+            boatReviewsCmp.refresh();
+        }
+
+    },
+
+    onBoatReviewAdded : function(component, event, helper) {
+
+        component.find("tabSet").set('v.selectedTabId', 'boatreviewtab');
+        var boatReviewsCmp = component.find("boatReviewsCmp");
+        if(boatReviewsCmp) {
+            boatReviewsCmp.refresh();
+        }
 
     }
+
+
 })
