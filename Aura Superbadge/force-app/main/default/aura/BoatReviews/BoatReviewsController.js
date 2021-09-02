6baef93433a2e2d987eb63bb4d6cb941f523ffda
Ryan Kramer <rkramer613@gmail.com>
2021-09-02 14:39:30 -0600

    Built out BoatReviews component

--- Aura Superbadge/force-app/main/default/aura/BoatReviews/BoatReviewsController.js
+++ Aura Superbadge/force-app/main/default/aura/BoatReviews/BoatReviewsController.js
@@ -1,5 +1,20 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit : function(component, event, helper) {
+            helper.onInit(component, event, helper);
+    },
 
+    onUserInfoClick : function(component, event, helper) {
+
+        var dataUserId = event.target.getAttribute("data-userid");
+        var redirectToUserEvent = $A.get("e.force:navigateToSObject");
+
+        if (redirectToUserEvent) {
+            redirectToUserEvent.setParams({
+                "recordId" : dataUserId
+            });
+            redirectToUserEvent.fire();
+        }
     }
+
+    
 })
